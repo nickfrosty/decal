607cfba78efd5fb05085578256ec6f2fb1a7cd31
nickfrosty <nfrostbutter@gmail.com>
2023-09-04 19:55:34 -0400

    feat: added styled touchable opacity

--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -1,5 +1,10 @@
+import { TouchableOpacity as DefaultTouchableOpacity } from "react-native-gesture-handler";
 import { Text, TextProps } from "./core/Themed";
 
 export function MonoText(props: TextProps) {
   return <Text {...props} style={[props.style, { fontFamily: "SpaceMono" }]} />;
+}
+
+export function TouchableOpacity(props: DefaultTouchableOpacity["props"]) {
+  return <DefaultTouchableOpacity {...props} activeOpacity={0.7} />;
 }
