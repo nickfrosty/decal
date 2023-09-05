2fac7ea22abbde75ccb8cb95834c2b73686d72ef
nickfrosty <nfrostbutter@gmail.com>
2023-09-05 19:26:12 -0400

    fix: forward ref on touchable opacity

--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -1,12 +1,16 @@
 import { TouchableOpacity as DefaultTouchableOpacity } from "react-native-gesture-handler";
 import { Text, TextProps } from "./core/Themed";
+import { forwardRef } from "react";
 
 export function MonoText(props: TextProps) {
   return <Text {...props} style={[props.style, { fontFamily: "SpaceMono" }]} />;
 }
 
-export function TouchableOpacity(props: DefaultTouchableOpacity["props"]) {
+export const TouchableOpacity = forwardRef<
+  DefaultTouchableOpacity,
+  DefaultTouchableOpacity["props"]
+>((props: DefaultTouchableOpacity["props"], ref) => {
   return <DefaultTouchableOpacity {...props} activeOpacity={0.7} />;
-}
+});
 
 export type TouchableOpacityProps = DefaultTouchableOpacity["props"];
