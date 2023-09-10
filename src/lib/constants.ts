183b705eb005016b63966e82b3f5d1362bc375ec
nickfrosty <nfrostbutter@gmail.com>
2023-09-10 08:57:42 -0400

    feat: added simple consts

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+/**
+ * Storage key values used to read and write specific data to local storage (aka `AsyncStorage`)
+ */
+export const STORAGE_KEY = {
+  connection: "KEY_NETWORK_CONNECTION",
+  userWallet: "KEY_SELECTED_USER_WALLET",
+};
