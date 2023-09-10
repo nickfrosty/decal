640735346606fdd75398bd9ab2ffe167dcc326bb
nickfrosty <nfrostbutter@gmail.com>
2023-09-10 08:59:28 -0400

    feat: added async storage based contexts

--- src/context/ConnectionProvider.tsx
+++ src/context/ConnectionProvider.tsx
@@ -2,10 +2,14 @@
  * NOTE: much of the boilerplate code her was taken from `@solana/wallet-adapter-react`
  */
 
+import { STORAGE_KEY } from "@/lib/constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   Connection,
   clusterApiUrl,
   type ConnectionConfig,
+  Cluster,
+  PublicKey,
 } from "@solana/web3.js";
 import React, {
   type FC,
@@ -13,34 +17,73 @@ import React, {
   useMemo,
   createContext,
   useContext,
+  useEffect,
+  useState,
 } from "react";
 
-export interface ConnectionProviderProps {
-  children: ReactNode;
-  endpoint?: string;
-  config?: ConnectionConfig;
+// set the default settings to load for the user before they
+const DEFAULT_SETTINGS: NetworkConnectionSettings = {
+  cluster: "devnet",
+  endpoint: clusterApiUrl("devnet"),
+  config: { commitment: "confirmed" },
+};
+
+type NetworkConnectionSettings = {
+  cluster: Cluster;
+  endpoint: string;
+  config: ConnectionConfig;
+};
+
+interface ConnectionContextState {
+  connection: Connection;
+  settings: NetworkConnectionSettings;
+  setSettings: React.Dispatch<React.SetStateAction<NetworkConnectionSettings>>;
+  userWallet: PublicKey;
+  setUserWallet: React.Dispatch<React.SetStateAction<PublicKey>>;
 }
 
-export const ConnectionProvider: FC<ConnectionProviderProps> = ({
-  children,
-  endpoint = clusterApiUrl("devnet"),
-  config = { commitment: "confirmed" },
-}) => {
+export const ConnectionProvider: FC<{
+  children: ReactNode;
+}> = ({ children }) => {
+  // track the user's connection settings, and allow updating them
+  const [settings, setSettings] =
+    useState<NetworkConnectionSettings>(DEFAULT_SETTINGS);
+  const [userWallet, setUserWallet] = useState<PublicKey>(new PublicKey(""));
+
+  // auto load the network connection configuration from local storage
+  useEffect(() => {
+    AsyncStorage.getItem(STORAGE_KEY.connection).then((data) => {
+      if (!!data) setSettings(JSON.parse(data));
+    });
+    AsyncStorage.getItem(STORAGE_KEY.userWallet).then((data) => {
+      if (!!data) setUserWallet(new PublicKey(data ?? ""));
+    });
+  }, []);
+
+  // auto save the settings when they are changed
+  useEffect(() => {
+    AsyncStorage.setItem(STORAGE_KEY.connection, JSON.stringify(settings));
+  }, [settings]);
+
+  // auto save the settings when they are changed
+  useEffect(() => {
+    AsyncStorage.setItem(STORAGE_KEY.userWallet, userWallet.toBase58());
+  }, [userWallet]);
+
+  // create a connection to the blockchain
   const connection = useMemo(
-    () => new Connection(endpoint, config),
-    [endpoint, config],
+    () => new Connection(settings.endpoint, settings.config),
+    [settings],
   );
 
   return (
-    <ConnectionContext.Provider value={{ connection }}>
+    <ConnectionContext.Provider
+      value={{ connection, settings, setSettings, userWallet, setUserWallet }}
+    >
       {children}
     </ConnectionContext.Provider>
   );
 };
-
-export interface ConnectionContextState {
-  connection: Connection;
-}
 
 export const ConnectionContext = createContext<ConnectionContextState>(
   {} as ConnectionContextState,
