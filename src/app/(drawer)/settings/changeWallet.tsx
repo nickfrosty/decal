8c82ec37443130113b002c675438b4937ccf6a5b
nickfrosty <nfrostbutter@gmail.com>
2023-09-10 14:18:54 -0400

    feat: actually changing the user's wallet

--- src/app/(drawer)/settings/changeWallet.tsx
+++ src/app/(drawer)/settings/changeWallet.tsx
@@ -1,9 +1,57 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { MinorText } from "@/components/core/Styled";
-import { ScrollView, MasterStyles } from "@/components/core/Themed";
-import { ListContainer, ListItemLink } from "@/components/core/ListContainer";
+import { ScrollView, MasterStyles, Text, View } from "@/components/core/Themed";
+import { ListContainer, ListCheckBox } from "@/components/core/ListContainer";
+import { useEffect, useState } from "react";
+import {
+  UserWalletDetails,
+  getAllUserWalletDetails,
+} from "@/lib/utils/wallet/details";
+import { useConnection } from "@/context/ConnectionProvider";
+import { shortText } from "@/lib/utils";
+import { Alert } from "react-native";
+
+/**
+ * todo: list of assorted things to add here
+ * - toggled text color on the account address text
+ * - show the label as well?
+ * - better handling of the toggle button displayed
+ */
 
 export default function Screen() {
+  const router = useRouter();
+  const { userWallet, setUserWallet } = useConnection();
+  const [accounts, setAccounts] = useState<UserWalletDetails[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const details = await getAllUserWalletDetails();
+      // console.log(details);
+      setAccounts(details);
+    })();
+
+    // return () => {};
+  }, []);
+
+  /**
+   * swap the user selected wallet
+   */
+  function handlePress(address: string) {
+    // locate the record from the list of accounts
+    const record = accounts.find((item) => item.address === address);
+
+    if (!record) {
+      return Alert.alert(
+        "Account not found",
+        "The selected account was not found.",
+      );
+    }
+
+    setUserWallet(record);
+
+    return router.back();
+  }
+
   return (
     <ScrollView style={MasterStyles.container}>
       <Stack.Screen
@@ -16,12 +64,19 @@ export default function Screen() {
       <MinorText className="text-base text-center">Select a Wallet</MinorText>
 
       <ListContainer>
-        <ListItemLink
-          isTop={true}
-          label="Change wallet"
-          href={"/settings/changeWallet"}
-        />
-        <ListItemLink label="Change wallet" href={"/settings/changeWallet"} />
+        {accounts.map((item, id) => (
+          <ListCheckBox
+            label=""
+            isTop={id == 0}
+            key={id}
+            isChecked={userWallet.address === item.address}
+            onPress={() => handlePress(item.address)}
+          >
+            <View className="bg-transparent">
+              <Text className="text-lg">{shortText(item.address)}</Text>
+            </View>
+          </ListCheckBox>
+        ))}
       </ListContainer>
     </ScrollView>
   );
