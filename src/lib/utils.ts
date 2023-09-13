9738d5a9ea8ec2575fd8eff339ae76bb31e2ed71
nickfrosty <nfrostbutter@gmail.com>
2023-09-12 21:55:23 -0400

    feat: added lamport math function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,8 @@
  *
  */
 
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
 /**
  *
  * todo: there is an edge case if the text.length of < `length`, then characters will be duplicated in the response
@@ -14,4 +16,14 @@ export function shortText(
   return (
     text.substring(0, length) + separator + text.substring(text.length - length)
   );
+}
+
+/**
+ * Convert a lamport number to a pretty to look at SOL value
+ */
+export function formatLamportsToSol(lamports: number) {
+  return Intl.NumberFormat(undefined, {
+    minimumSignificantDigits: 1,
+    minimumFractionDigits: 3,
+  }).format(lamports / LAMPORTS_PER_SOL);
 }
