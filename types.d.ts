f4e37b4eea3c94c8b6d7fca16b9b17a6f88f1b53
nickfrosty <nfrostbutter@gmail.com>
2023-09-13 19:17:27 -0400

    feat: added global types and env types

---
+++ types.d.ts
@@ -0,0 +1,29 @@
+/**
+ * Global types for use throughout the repo
+ */
+
+type Option<T> = T | null;
+
+/**
+ * Define simple type definitions of the env variables
+ */
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    /**
+     * General variables and settings
+     */
+    // API_URL: string;
+
+    /**
+     * Solana specific variables
+     */
+    EXPO_PUBLIC_SOLANA_RPC_MONIKER?: string;
+    EXPO_PUBLIC_SOLANA_RPC_URL?: string;
+
+    /**
+     * Assorted variables used for testing and debugging only
+     */
+    EXPO_PUBLIC_DEBUG_SEED_PHRASE?: string;
+    EXPO_PUBLIC_DEBUG_SEED_WORD?: string;
+  }
+}
