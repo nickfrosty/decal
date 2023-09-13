4f3288dbaa14fd109a950c8695b326ece5e218a3
nickfrosty <nfrostbutter@gmail.com>
2023-09-13 18:36:30 -0400

    feat: scaffold welcome screen

---
+++ src/app/welcome.tsx
@@ -0,0 +1,27 @@
+import { Stack } from "expo-router";
+import DefaultLayout from "@/components/core/DefaultLayout";
+
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { View } from "@/components/core/Themed";
+import { GhostLink } from "@/components/core/buttons";
+
+export default function Screen() {
+  return (
+    <DefaultLayout>
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          // comment for better diffs
+          title: "Welcome to Decal 🎉",
+        }}
+      />
+
+      <View className="flex flex-row items-center justify-center my-8">
+        <GhostLink href="/wallet/import" label="Import" />
+        <GhostLink href="/wallet/generate" label="Generate" />
+      </View>
+
+      <EditScreenInfo path="app/welcome.tsx" />
+    </DefaultLayout>
+  );
+}
