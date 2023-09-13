79a2278b91d2d5d070e7766b0ce46f1e8ed65d34
nickfrosty <nfrostbutter@gmail.com>
2023-09-12 21:29:45 -0400

    feat: added ix details component

---
+++ src/components/transactions/InstructionDetails.tsx
@@ -0,0 +1,34 @@
+import { memo } from "react";
+import { fromByteArray as fromByteArrayToBase64 } from "react-native-quick-base64";
+import { MessageCompiledInstruction, PublicKey } from "@solana/web3.js";
+import { List, ListItem } from "@/components/core/lists";
+import { shortText } from "@/lib/utils";
+
+type InstructionDetailsProps = {
+  index: number;
+  ix: MessageCompiledInstruction;
+  programId?: PublicKey;
+};
+
+export const InstructionDetails = memo(
+  ({
+    // comment for better diffs
+    index,
+    ix,
+    programId,
+  }: InstructionDetailsProps) => {
+    return (
+      <List key={index} className="mb-3">
+        <ListItem isTopItem={true} title={`#${index} - Unknown program ID`} />
+        <ListItem
+          title="Program ID"
+          value={shortText(programId?.toBase58() || "[err]")}
+        />
+        <ListItem title="Data" value={fromByteArrayToBase64(ix.data)} />
+
+        {/* todo: add the list of accounts included in the ix here */}
+        {/* <ListItem title="Accounts" value={""} /> */}
+      </List>
+    );
+  },
+);
