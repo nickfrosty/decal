130385acdc334a4d2a41d03aa9d96313ee4a6c7f
nickfrosty <nfrostbutter@gmail.com>
2023-09-18 21:06:04 -0400

    refactor: use modal

--- src/app/(wallet)/dev.tsx
+++ src/app/(wallet)/dev.tsx
@@ -2,9 +2,49 @@ import { Stack } from "expo-router";
 import DefaultLayout from "@/components/core/DefaultLayout";
 
 import { Text, View } from "@/components/core/Themed";
-import { GhostLink } from "@/components/core/buttons";
+import { GhostButton, GhostLink } from "@/components/core/buttons";
+import { useTransaction } from "@/context/TransactionProvider";
+import {
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  TransactionMessage,
+  VersionedTransaction,
+} from "@solana/web3.js";
+
+// const wallet = Keypair.generate().publicKey;
+const wallet = new PublicKey("Dp1Je1aJ9zifmHpbe64bbLabMqFsAsLnXnSsNSiNsNWL");
+console.log("wallet:", wallet.toBase58());
+
+const receiver = Keypair.generate().publicKey;
+// const receiver = new PublicKey("GQuioVe2yA6KZfstgmirAvugfUZBcdxSi7sHK7JGk3gk");
+console.log("receiver:", receiver.toBase58());
+
+const transaction = new VersionedTransaction(
+  new TransactionMessage({
+    payerKey: wallet,
+    instructions: [
+      // ix - #1
+      SystemProgram.transfer({
+        fromPubkey: wallet,
+        lamports: 10_000_000,
+        toPubkey: receiver,
+      }),
+      // ix - #2
+      // SystemProgram.transfer({
+      //   fromPubkey: wallet,
+      //   lamports: 1,
+      //   toPubkey: Keypair.generate().publicKey,
+      // }),
+    ],
+    // todo: I am intentionally setting no value
+    recentBlockhash: "",
+  }).compileToV0Message(),
+);
 
 export default function Screen() {
+  const { triggerTransactionRequest } = useTransaction();
+
   return (
     <DefaultLayout>
       <View className={"flex-1 p-4 gap-8 items-center"}>
@@ -17,7 +57,10 @@ export default function Screen() {
         <View className="flex flex-row items-center justify-center gap-2">
           <GhostLink href="/transaction/details" label="Details" />
           <GhostLink href="/transaction/signMessage" label="Sign" />
-          <GhostLink href="/transaction/requestApproval" label="Request" />
+          <GhostButton
+            label="Request"
+            onPress={() => triggerTransactionRequest(transaction)}
+          />
         </View>
 
         <View
