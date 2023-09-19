1528db65c5b8112a6d90be20965b99d2ea773143
nickfrosty <nfrostbutter@gmail.com>
2023-09-18 21:05:45 -0400

    feat: transaction modal

--- src/components/modals/TransactionModal.tsx
+++ src/components/modals/TransactionModal.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useMemo, useState } from "react";
 import {
   BottomModal,
   BottomModalProps,
@@ -7,7 +7,30 @@ import {
 import { HeroIcon, HeroTitleSection } from "@/components/ScreenHero";
 import { QuestionMarkCircleIcon } from "react-native-heroicons/outline";
 
-export const TransactionModal = ({ modalRef }: BottomModalProps) => {
+import { Text, View } from "@/components/core/Themed";
+import { List, ListItem } from "@/components/core/lists";
+import { VersionedTransaction } from "@solana/web3.js";
+import { formatLamportsToSol, shortText } from "@/lib/utils";
+import { MinorText, TouchableOpacity, ViewBox } from "@/components/core/Styled";
+import { Button, GhostButton } from "@/components/core/buttons";
+// import { useSimulateTransaction } from "@/hooks/useSimulateTransaction";
+import { Alert } from "react-native";
+import {
+  ChevronDoubleDownIcon,
+  ChevronDoubleUpIcon,
+} from "react-native-heroicons/solid";
+import { InstructionDetails } from "@/components/transactions/InstructionDetails";
+
+type ModalProps = BottomModalProps & {
+  transaction: VersionedTransaction | null;
+  simulation: any;
+};
+
+export const TransactionModal = ({
+  modalRef,
+  transaction,
+  simulation,
+}: ModalProps) => {
   /**
    * Callback function to close the modal
    */
@@ -15,24 +38,111 @@ export const TransactionModal = ({ modalRef }: BottomModalProps) => {
     modalRef.current?.dismiss();
   }, []);
 
+  // track some react state
+  const [showAdvancedDetails, setShowAdvancedDetails] = useState(false);
+
+  // memoize a nice label for the fee payer
+  const feePayerLabel = useMemo(() => {
+    const addr = simulation?.accountAddresses[0] || "[err]";
+
+    // todo: this
+    // if (addr === wallet.toBase58()) return "you";
+    // else
+    return shortText(addr);
+  }, [transaction]);
+
   return (
-    <BottomModal modalRef={modalRef}>
+    <BottomModal modalRef={modalRef} snapPoints={["90%"]}>
       <ModalHeader
         handleCloseModal={closeModal}
-        title="Transaction Request"
+        // title={"Transaction Request"}
         // description="Change your active wallet account"
       />
 
-      <HeroIcon
-        background="bg-gray-300"
-        icon={<QuestionMarkCircleIcon size={40} color={"white"} />}
-        label="decal.app"
-      />
+      <View className="mb-4 bg-transparent">
+        <HeroIcon
+          background="bg-gray-300"
+          icon={<QuestionMarkCircleIcon size={40} color={"white"} />}
+          label="decal.app"
+        />
 
-      <HeroTitleSection
-        title={"Transaction Request"}
-        description={"You are being asked to approve a transaction"}
-      />
+        <HeroTitleSection
+          title={"Transaction Request"}
+          description={"You are being asked to approve a transaction"}
+        />
+      </View>
+
+      {/* todo: this should be themed */}
+      {!!simulation?.error && (
+        <ViewBox className="bg-red-500 border-red-700">
+          <Text className="text-base">
+            If approved, this transaction is still likely to fail!
+          </Text>
+        </ViewBox>
+      )}
+
+      {/* simple transaction details */}
+      <List>
+        {/* <ListItem isTopItem={true} title="Network" value={"Solana mainnet"} /> */}
+        <ListItem
+          isTopItem={true}
+          title="Network fee"
+          value={`${formatLamportsToSol(simulation.estimatedFee)} SOL`}
+        />
+        <ListItem title="Fee payer" value={feePayerLabel} />
+      </List>
+
+      {/* transaction and instruction details section */}
+      <TouchableOpacity
+        onPress={() => setShowAdvancedDetails((prev) => !prev)}
+        className="flex flex-row items-center justify-between mt-6 bg-transparent"
+      >
+        <MinorText className={"w-full text-center text-base"}>
+          Toggle advanced details
+          {/* {showAdvancedDetails
+            ? "Toggle advanced details"
+            : "Show advanced details?"} */}
+        </MinorText>
+
+        {/* <View className="p-2 bg-transparent">
+          {showAdvancedDetails ? (
+            <ChevronDoubleUpIcon className="" size={20} color={"white"} />
+          ) : (
+            <ChevronDoubleDownIcon className="" size={20} color={"white"} />
+          )}
+        </View> */}
+      </TouchableOpacity>
+
+      {/* process each of the instructions for display */}
+      {showAdvancedDetails && (
+        <View className="flex flex-col bg-transparent">
+          {transaction?.message.compiledInstructions.map((ix, key) => (
+            <InstructionDetails
+              key={key}
+              index={key}
+              ix={ix}
+              programId={simulation.accountAddresses[ix.programIdIndex]}
+            />
+          ))}
+        </View>
+      )}
+
+      {/* button action area */}
+      <View className="flex flex-col gap-3 my-4 bg-transparent">
+        <Button
+          label="Approve"
+          onPress={() => Alert.alert("approve")}
+          className="w-full bg-blue-500"
+          labelClassName="text-white"
+        />
+        <GhostButton
+          label="Reject"
+          onPress={() => Alert.alert("reject")}
+          // className=""
+          labelClassName="text-base"
+          // icon="external-link"
+        />
+      </View>
     </BottomModal>
   );
 };
