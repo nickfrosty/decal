b2c89a419df50a849677f80bf8be71c9a4ea5175
nickfrosty <nfrostbutter@gmail.com>
2023-09-19 22:33:02 -0400

    fix: styles

--- src/components/transactions/InstructionDetails.tsx
+++ src/components/transactions/InstructionDetails.tsx
@@ -1,13 +1,16 @@
 import { memo } from "react";
 import { fromByteArray as fromByteArrayToBase64 } from "react-native-quick-base64";
-import { MessageCompiledInstruction, PublicKey } from "@solana/web3.js";
+import { MessageCompiledInstruction } from "@solana/web3.js";
 import { List, ListItem } from "@/components/core/lists";
 import { shortText } from "@/lib/utils";
+import { StyleProp, ViewStyle } from "react-native";
 
 type InstructionDetailsProps = {
   index: number;
   ix: MessageCompiledInstruction;
   programId: string;
+  style?: StyleProp<ViewStyle>;
+  className?: string;
 };
 
 export const InstructionDetails = memo(
@@ -16,9 +19,10 @@ export const InstructionDetails = memo(
     index,
     ix,
     programId,
+    style,
   }: InstructionDetailsProps) => {
     return (
-      <List key={index} className="mb-3">
+      <List key={index} className="mb-3" style={style}>
         <ListItem isTopItem={true} title={`#${index} - Unknown program ID`} />
         <ListItem title="Program ID" value={shortText(programId || "[err]")} />
         <ListItem title="Data" value={fromByteArrayToBase64(ix.data)} />
